5570e38fc14171f15e9365ad055b071700221274
Anil Soni <anil.soni.us@outlook.com>
2021-11-13 21:03:41 +0000

    Changes for rpi

---
+++ setup.sh
@@ -0,0 +1,9 @@
+rm -r tools/dex2jar
+unzip ~/dex2jar-2.1.zip -d tools/
+mv tools/dex-tools-2.1 tools/dex2jar
+
+chmod 744 apk_decompiler.py
+chmod 744 tools/jd-cli/jd*
+chmod 744 tools/apktool/*
+
+
